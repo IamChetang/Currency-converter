be60002fefc8f070d70be43cddb81b0c1d8038e4
IamChetang <chetang568568@gmail.com>
2021-09-19 18:20:15 +0530

    final commit

--- scripts/script.js
+++ scripts/script.js
@@ -4,25 +4,25 @@ const amountOneEl = document.getElementById('amount-one');
 const currencyTwoEl = document.getElementById('currency-two');
 const amountTwoEl = document.getElementById('amount-two');
 
-// // function
-// const calculate = function () {
-//   const currencyOne = currencyOneEl.value;
-//   const currencyTwo = currencyTwoEl.value;
+// function
+const calculate = function () {
+  const currencyOne = currencyOneEl.value;
+  const currencyTwo = currencyTwoEl.value;
 
-//   const url = `https://open.er-api.com/v6/latest/${currencyOne}`;
-//   fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       const rate = data.rates[currencyTwo];
-//       amountTwoEl.value = (amountOneEl.value * rate).toFixed(2);
-//     });
-// };
+  const url = `https://open.er-api.com/v6/latest/${currencyOne}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const rate = data.rates[currencyTwo];
+      amountTwoEl.value = (amountOneEl.value * rate).toFixed(2);
+    });
+};
 
-// // event listeners
-// currencyOneEl.addEventListener('change', calculate);
-// amountOneEl.addEventListener('input', calculate);
-// currencyTwoEl.addEventListener('change', calculate);
-// amountTwoEl.addEventListener('input', calculate);
+// event listeners
+currencyOneEl.addEventListener('change', calculate);
+amountOneEl.addEventListener('input', calculate);
+currencyTwoEl.addEventListener('change', calculate);
+amountTwoEl.addEventListener('input', calculate);
 
-// // initial settings
-// calculate();
+// initial settings
+calculate();
